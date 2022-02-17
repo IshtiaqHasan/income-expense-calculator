103d55a6ca1371d3c70cefb07ecc6ec4fdeba526
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-02-17 19:24:21 +0600

    savings-added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,79 @@
 document.getElementById('expense-btn').addEventListener('click', function () {
-    const food = document.getElementById('food-expense');
-    const foodCost = food.value;
-    const foodExpense = parseFloat(foodCost);
+    expenses();
+    errorHandle();
+    const inputMoney = document.getElementById('income').value;
+    const foodCost = document.getElementById('food-expense').value;
+    const rentCost = document.getElementById('rent-expense').value;
+    const clothCost = document.getElementById('cloth-expense').value;
 
-    const rent = document.getElementById('rent-expense');
-    const rentCost = rent.value;
-    const rentExpense = parseFloat(rentCost);
+    if (foodCost > inputMoney) {
+        const message = document.getElementById('add-correctValue')
+        message.style.display = 'block';
+    }
+    if (rentCost > inputMoney) {
+        const message = document.getElementById('add-correctValue')
+        message.style.display = 'block';
+    }
+    if (clothCost > inputMoney) {
+        const message = document.getElementById('add-correctValue')
+        message.style.display = 'block';
+    }
 
-    const cloth = document.getElementById('cloth-expense');
-    const clothCost = cloth.value;
-    const clothExpense = parseFloat(clothCost);
 
-    const total = foodExpense + rentExpense + clothExpense;
-    console.log(total)
+});
 
-    const allCost = document.getElementById('total-expenses');
-    //const totalExpense = allCost.innerText;
-    allCost.innerText = total;
+document.getElementById('saving-button').addEventListener('click', function () {
+    expenses();
+    errorHandle();
+    const inputMoney = document.getElementById('income').value;
+    const savingAmount = document.getElementById('saving-amount').innerText;
 
-})
+    if (savingAmount > inputMoney / 2) {
+        const message = document.getElementById('saving-warning')
+        message.style.display = 'block';
+    }
+
+});
+
+
+function expenses() {
+
+    // all cost and expenses
+    const foodCost = document.getElementById('food-expense').value;
+
+    const rentCost = document.getElementById('rent-expense').value;
+
+    const clothCost = document.getElementById('cloth-expense').value;
+
+    const totalCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothCost);
+    const totalExpenses = document.getElementById('total-expenses');
+    totalExpenses.innerText = totalCost;
+
+
+    // income & balance section
+    const income = document.getElementById('income').value;
+    const incomeAmount = parseFloat(income);
+    const balance = document.getElementById('balance');
+    const balanceText = balance.innerText;
+
+    const totalBalance = incomeAmount - totalCost;
+    balance.innerText = totalBalance;
+
+    // saving
+    const percentageInput = document.getElementById('percentage-input');
+    let percentage = parseFloat(percentageInput.value);
+
+    const saving = ((incomeAmount * percentage) / 100);
+
+    const savingAmount = document.getElementById('saving-amount');
+    const savingAmountText = savingAmount.innerText;
+    savingAmount.innerText = saving;
+
+    const remainingBalance = document.getElementById('remaining-balance');
+    const remainingBalanceText = remainingBalance.innerText;
+    const remainingBlanceAmount = totalBalance - saving;
+    remainingBalance.innerText = remainingBlanceAmount;
+
+}
+
+
